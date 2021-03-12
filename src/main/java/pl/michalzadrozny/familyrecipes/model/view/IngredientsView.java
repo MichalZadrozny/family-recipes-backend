9198da320479cf2e65486b45c8c417051a41eeeb
Michał Zadrożny <mt.zadrozny@gmail.com>
2021-03-12 12:28:57 +0100

    Add RecipeView and IngredientsView

---
+++ src/main/java/pl/michalzadrozny/familyrecipes/model/view/IngredientsView.java
@@ -0,0 +1,10 @@
+package pl.michalzadrozny.familyrecipes.model.view;
+
+public interface IngredientsView {
+
+    Double getAmount();
+
+    String getName();
+
+    String getUnit();
+}
---
+++ src/main/java/pl/michalzadrozny/familyrecipes/model/view/RecipeView.java
@@ -0,0 +1,34 @@
+package pl.michalzadrozny.familyrecipes.model.view;
+
+import org.springframework.beans.factory.annotation.Value;
+import pl.michalzadrozny.familyrecipes.model.entity.Diet;
+import pl.michalzadrozny.familyrecipes.model.entity.Nutrients;
+
+import java.util.List;
+
+public interface RecipeView {
+
+    Long getId();
+
+    String getName();
+
+    String getDescription();
+
+    int getPreparationTime();
+
+    @Value("#{target.author.username}")
+    String getUsername();
+
+    @Value("#{target.rating.averageRating}")
+    double getAverageRating();
+
+    @Value("#{target.rating.ratingsMap.size()}")
+    int getNumberOfRatings();
+
+    Diet getDiet();
+
+    Nutrients getNutrients();
+
+    List<IngredientsView> getIngredients();
+
+}
