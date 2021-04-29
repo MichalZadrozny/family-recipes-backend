3347b2a9ed7fb696507710f2de0243c4aeb7b386
Michał Zadrożny <mt.zadrozny@gmail.com>
2021-04-29 12:16:39 +0200

    Make successfulAuthentication return id & username

--- src/main/java/pl/michalzadrozny/familyrecipes/security/JWTAuthenticationFilter.java
+++ src/main/java/pl/michalzadrozny/familyrecipes/security/JWTAuthenticationFilter.java
@@ -3,14 +3,15 @@ package pl.michalzadrozny.familyrecipes.security;
 import com.auth0.jwt.JWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import pl.michalzadrozny.familyrecipes.model.mapper.UserMapper;
 import pl.michalzadrozny.familyrecipes.model.dto.LoginDTO;
 import pl.michalzadrozny.familyrecipes.model.entity.AppUser;
+import pl.michalzadrozny.familyrecipes.model.mapper.UserMapper;
 
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +19,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 import static pl.michalzadrozny.familyrecipes.security.SecurityConstants.EXPIRATION_TIME;
@@ -55,14 +58,20 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
+
+        AppUser user = (AppUser) authResult.getPrincipal();
+
         String token = JWT.create()
-                .withSubject(((AppUser) authResult.getPrincipal()).getUsername())
+                .withSubject(user.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .sign(HMAC512(SECRET.getBytes()));
 
-        String json = "{\"token\":\"" + token + "\"}";
+        Map<String, String> jsonMap = new HashMap<>();
+        jsonMap.put("token", token);
+        jsonMap.put("username", user.getUsername());
+        jsonMap.put("userId", user.getId().toString());
 
-        response.getWriter().write(json);
+        response.getWriter().write(new JSONObject(jsonMap).toString());
         response.setContentType("application/json");
         response.getWriter().flush();
     }
