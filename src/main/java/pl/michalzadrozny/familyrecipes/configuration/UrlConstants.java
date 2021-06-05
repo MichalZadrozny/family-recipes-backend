b5b08e393d4e6f25263253b781a77c2d64cc4fd0
Michał Zadrożny <mt.zadrozny@gmail.com>
2021-06-05 12:12:01 +0200

    Fix redirects

--- src/main/java/pl/michalzadrozny/familyrecipes/configuration/UrlConstants.java
+++ src/main/java/pl/michalzadrozny/familyrecipes/configuration/UrlConstants.java
@@ -1,9 +1,13 @@
 package pl.michalzadrozny.familyrecipes.configuration;
 
 public class UrlConstants {
+
+    private UrlConstants() {
+    }
+
     public static final String[] SWAGGER_URLS = {"/swagger-ui.html", "/v2/api-docs", "/swagger-resources/**", "/webjars/**"};
 
-    public static final String WEBSITE_URL = "https://family-recipes-prod.herokuapp.com/";
+    public static final String WEBSITE_URL = "https://family-recipes-prod.herokuapp.com";
     public static final String WEBSITE_LOGIN_URL = WEBSITE_URL + "/login";
     public static final String WEBSITE_RECOVERY_URL = WEBSITE_URL + "/recover-password";
 }
